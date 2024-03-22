5df2b1f4cf201e56f44c591b7b9f77f23f3e49e8
J0shDennis <141744139+J0shDennis@users.noreply.github.com>
2024-03-22 08:30:16 -0500

    g

---
+++ datatypes.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$name = 'coding is fun';
+$name = '30';
+$name = '20.34';
+$names = array("more","jim","clark","joshua","deni");
+ echo $names['3'];
+
+?>
+</body>
+</html>
