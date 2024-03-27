e287850b7f39e7e179add34c85329928bf87ec41
J0shDennis <141744139+J0shDennis@users.noreply.github.com>
2024-03-27 08:00:33 -0500

    g

---
+++ 2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /*
+$test = 5;
+for($i = $test; $i <= 10; $i++){
+echo "this is " . $i . "<br>";
+}*/
+    /*
+
+$test = 5;
+do {
+    echo $test;
+    $test++;
+} while ($test < 10);
+*/
+$fruit = array("apple","Banana","orange");
+
+
+
+
+
+
+
+
+?>
+</body>
+</html>
