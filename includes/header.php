b2c97d82917b8c9e20a0f7c8af369b29cc5dafb8
J0shDennis <141744139+J0shDennis@users.noreply.github.com>
2024-04-04 08:07:44 -0500

    g

---
+++ includes/header.php
@@ -0,0 +1,44 @@
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="calculator.php">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="2.php">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="calendar.php">calendar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="datatypes.php">datatypes</a>
+        </li>        
+        <li class="nav-item">
+          <a class="nav-link" href="functions.php">functions</a>
+        </li>        
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">index</a>
+        </li>        
+        <li class="nav-item">
+          <a class="nav-link" href="loop.php">loop</a>
+        </li>        
+        <li class="nav-item">
+          <a class="nav-link" href="next.php">next</a>
+        </li>        
+        <li class="nav-item">
+          <a class="nav-link" href="operators.php">operators</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
