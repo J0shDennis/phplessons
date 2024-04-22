80d7aae56dae01ac7e1c78c29e1791cb911436f4
J0shDennis <141744139+J0shDennis@users.noreply.github.com>
2024-04-22 08:38:14 -0500

    g

---
+++ includes2/headernew.php
@@ -0,0 +1,37 @@
+<html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="desctiption" content="This is an example of a meta description. This will often show up in search results.">
+            <meta name="viewport content="width=device-width, initial-scale=1">
+            <tiele></title>
+            
+</head>
+<body>
+    <header>
+<nav>
+<a href="#">
+<img src="img/logo.png" alt="logo">
+</a>
+<ul>
+    <li><a href="index2.php"></a>Home</li>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Home</a></li>
+</ul>
+<div>
+    <form action="includes2/login.inc.php" method="post">
+        <input type="text" name="mailude" placeholder="Username/E-mail...">
+        <input type="pasword" name="pwd" placeholder="Password...">
+        <button type="submit" name="login-submit">Login</button>
+</form>
+<a href="signup.php">Signup</a>
+<form action="includes2/logout.inc.php" method="post">
+        <button type="submit" name="logout-submit">Logout</button>
+</form>
+<a href="signup.php">Signup</a>
+</div>
+</nav>
+</header>
+
+
---
+++ index2.php
@@ -0,0 +1,10 @@
+<?PHP_ROUND_HALF_DOWNrequire "header.php";
+?>
+
+<main>
+<p>You are logged out</p>
+<p>You are logged in</p>
+</main>
+
+<?phprequire "footer.php";
+?>
